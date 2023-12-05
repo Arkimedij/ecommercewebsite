db81f7368925f1099e65dacc9f23eeb482252039
Arkimedij <121115757+Arkimedij@users.noreply.github.com>
2023-12-05 08:45:56 +0545

    second commit

--- ecomproject/urls.py
+++ ecomproject/urls.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include 
-from django import settings
-from django.conf.urls.static import static
-
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("ecomapp.urls")),
+    path('ecom',include('ecomapp.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
-
